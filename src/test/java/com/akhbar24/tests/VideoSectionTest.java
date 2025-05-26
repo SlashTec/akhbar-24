2698dc8027c8176737a034101b634a3374485ef6
SlashTec <bash@slashtec.com>
2025-05-26 22:23:13 +0300

    update

--- src/test/java/com/akhbar24/tests/VideoSectionTest.java
+++ src/test/java/com/akhbar24/tests/VideoSectionTest.java
@@ -80,7 +80,7 @@ public class VideoSectionTest extends BaseTest {
                 WebElement freshVideoIndicator = driver.findElement(videoIndicatorLocator);
                 Assert.assertTrue(freshVideoIndicator.isDisplayed(), "❌ الفيديو لم يظهر أو لم يبدأ التشغيل.");
             } catch (Exception e) {
-               
+
                 Assert.fail("❌ حدث خطأ أثناء اختبار / المرئيلات _ الفيديو: " + e.getMessage());
             }
         }
